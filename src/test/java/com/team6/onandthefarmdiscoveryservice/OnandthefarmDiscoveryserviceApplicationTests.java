7d2d7cdbf2d5f5d71c8441a5e0992a7013ce2371
ksh940925 <ksh9409255@naver.com>
2022-10-04 20:01:45 +0900

    feat: init setting

---
+++ src/test/java/com/team6/onandthefarmdiscoveryservice/OnandthefarmDiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team6.onandthefarmdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnandthefarmDiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
