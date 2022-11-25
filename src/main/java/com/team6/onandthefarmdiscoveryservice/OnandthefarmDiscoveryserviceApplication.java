1958dc30079e224219d1471ae2383c479c7eadf0
이석원 <clalsw@seokwon-MacBookPro.local>
2022-11-25 18:11:00 +0900

    hotfix/핫픽수

--- src/main/java/com/team6/onandthefarmdiscoveryservice/OnandthefarmDiscoveryserviceApplication.java
+++ src/main/java/com/team6/onandthefarmdiscoveryservice/OnandthefarmDiscoveryserviceApplication.java
@@ -2,8 +2,10 @@ package com.team6.onandthefarmdiscoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class OnandthefarmDiscoveryserviceApplication {
 
 	public static void main(String[] args) {
